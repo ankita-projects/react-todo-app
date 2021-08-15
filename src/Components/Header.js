094de72826c97eb21c163ef84ae8f8e414ce29a1
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-08-15 17:13:57 +0200

    updating readme

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,10 @@ import React from "react";
 const Header = ({taskList}) =>{
     return (
         <header>
+            <div className="main">
             <h1>Task Master</h1>
             <p>I have currently {taskList.length} notes </p>
+            </div>
         </header>
     );
 } ;
--- src/Components/ListNote.js
+++ src/Components/ListNote.js
@@ -7,10 +7,14 @@ const ListNote = ({taskList,completeTaskHandler}) =>{
      <ul>
         {taskList.map(function(task, index){
                     return (
-                    <div>
+                    <div className ="wrape">
+                      <div className="icon">
                       <i class="fas fa-trash-alt"></i>
+                      </div>
                    <button type="button" data={ index } onClick={() => completeTaskHandler(index)}><FontAwesomeIcon  data={ index } icon={faTrash} /></button>
+                    <div className="list">
                     <li key={ index } style={{ textDecoration: task.isComplete == "true" ? "line-through":"none"}}>{task.name} </li>
+                    </div>
                     </div>
                     );
                   })}
